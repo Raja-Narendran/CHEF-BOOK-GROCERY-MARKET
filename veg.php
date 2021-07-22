760b6bd7f1003fc4928f270eb951f73cb5dc51f8
Raja-Narendran <47933008+Raja-Narendran@users.noreply.github.com>
2021-07-22 22:03:03 +0530

    Create veg.php

---
+++ veg.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ParCan
+ * Date: 26-05-2020
+ * Time: 02:37 PM
+ */
+?>
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="./fonts/materialize/material-icons.css"/>
+    <link type="text/css" rel="stylesheet" href="./plugins/materialize/css/materialize.min.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+    <link type="text/css" rel="stylesheet" href="./plugins/materialize/css/materialize.css"/>
+    <link type="text/css" rel="stylesheet" href="./fonts/materialize/material-icons.css"/>
+    <link type="text/css" rel="stylesheet" href="./css/home.css"/>
+    <style>
+        .bordedbox {
+            border-radius: 20px;
+            background: darkgray;
+            width: 12%;
+            height:40%;
+            float: left;
+            margin: 4%;
+            display: inline-block;
+        }
+        img{
+            display: inline-block;
+            width: 100%;
+            height: 100%;
+        }
+        .card-img-properties{
+            width: 100%;
+            height: 50%;
+        }
+        .dishName{
+            overflow: auto;
+            height: 20%;
+        }
+        .dish-div {
+            padding: 1%;
+            margin: 1%;
+        }
+        .card-properties {
+            float: left;
+            margin: 3%;
+        }
+    </style>
+</head>
+<body>
+<h1>
+    Vegetable
+</h1>
+<div style="float: left">
+<div class="card hoverable card-properties" style="width: 20%" >
+    <div class="card-image card-img-properties waves-effect waves-block waves-light">
+        <img class="activator" src="images/img4.jpg">
+    </div>
+    <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+</div>
+<div class="card hoverable card-properties" style="width: 20%">
+    <div class="card-image card-img-properties waves-effect waves-block waves-light">
+        <img class="activator" src="images/img1.jpg">
+    </div>
+    <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+</div>
+    <div class="card hoverable card-properties" style="width: 20%" >
+        <div class="card-image card-img-properties waves-effect waves-block waves-light">
+            <img class="activator" src="images/img4.jpg">
+        </div>
+        <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+        </div>
+        <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+    </div>
+    <div class="card hoverable card-properties" style="width: 20%">
+        <div class="card-image card-img-properties waves-effect waves-block waves-light">
+            <img class="activator" src="images/img1.jpg">
+        </div>
+        <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+        </div>
+        <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+    </div>
+    <div class="card hoverable card-properties" style="width: 20%" >
+        <div class="card-image card-img-properties waves-effect waves-block waves-light">
+            <img class="activator" src="images/img4.jpg">
+        </div>
+        <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+        </div>
+        <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+    </div>
+    <div class="card hoverable card-properties" style="width: 20%">
+        <div class="card-image card-img-properties waves-effect waves-block waves-light">
+            <img class="activator" src="images/img1.jpg">
+        </div>
+        <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+            <p><a href="#">This is a link</a></p>
+        </div>
+        <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+    </div>
+    <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">+</i></a>
+</div>
+</body>
+</html>
